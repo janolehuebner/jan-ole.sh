ec67983b5e53b308969093db0ee5aead4f5a2f88
Jan-Ole Hübner <jan-ole@huebner.pink>
2025-04-04 20:01:11 +0200

    feat: add os detection

--- main.sh
+++ main.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/env bash
-echo IT WORKS
+
+detect_os() {
+    case "$OSTYPE" in
+        solaris*) echo "solaris" ;;
+        darwin*)  echo "macos" ;;
+        linux*)   echo "linux" ;;
+        bsd*)     echo "bsd" ;;
+        msys* | cygwin*) echo "windows" ;;
+        *)        echo "unknown" ;;
+    esac
+}
+
+echo "You are on $(detect_os)"
+
